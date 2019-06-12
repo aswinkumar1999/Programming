57823466666c6e8514b777c555090b189910c7db
Aswinkumar <aswinkumar1999@gmail.com>
2019-06-12 06:08:42 +0530

    template for cc

---
+++ C++/template.cpp
@@ -0,0 +1,20 @@
+//--------------------------------------------------------------
+//Author :
+//Date :
+//Website :
+//--------------------------------------------------------------
+#include<bits/stdc++.h>
+#define INF 1000000007
+using namespace std;
+
+int main(){
+  //Used for Fast I/O
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  while(t--){
+    //The Code Goes here
+  }
+  return 0;
+}
